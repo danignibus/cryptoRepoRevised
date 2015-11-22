9d1e383ded05edbe9a4fe4c000ff727a75d95cdb
Dani Gnibus <Danielle.N.Gnibus.17@dartmouth.edu>
2015-11-22 23:42:41 +0100

    communication between GUI and upload class solidified

--- src/main/java/cryptomasters/ApplicationGui.java
+++ src/main/java/cryptomasters/ApplicationGui.java
@@ -92,7 +92,8 @@ public class ApplicationGui extends javax.swing.JFrame {
     private void uploadButtonActionPerformed(java.awt.event.ActionEvent evt) {                                             
         // TODO add your handling code here:
         System.out.println("Upload selected");
-        HttpsSendUpload.main(new String[0]);
+      //  String [] args = new String ["hello", "hello", "hello", "hello", "]
+        HttpsSendUpload.main("hello", "hello", "hello1", "cryptocook.png", "/Users/danignibus/Desktop/cryptocook.png");
         
     }                                            
 
